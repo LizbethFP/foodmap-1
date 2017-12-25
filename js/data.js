0da8945473f568022fba1670fd2ed1401ccc86e9
Cintia Tahirih <tahysluana@gmail.com>
2017-12-25 16:40:59 -0500

    modifica data

--- js/data.js
+++ js/data.js
@@ -1,278 +1,272 @@
 var data = {
-  'MEXICO': {
-    'restaurants': [
-      {
-        'name': 'Taqueria No Te Rajes',
-        'location': {
-          'coordinate x': 90,
-          'coordinate y': 3
-        },
-        'menu': [
-          {
-            'name': 'Taco de chorizo',
-            'price': 7,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Tacos al Pastor',
-            'price': 15,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Elote',
-            'price': 10,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          }
-        ]
+  'MEXICO': [
+    {
+      'name': 'Taqueria No Te Rajes',
+      'location': {
+        'coordinate x': 90,
+        'coordinate y': 3
       },
-      {
-        'name': 'Como Agua para Chocolate',
-        'location': {
-          'coordinate x': 90,
-          'coordinate y': 3
+      'menu': [
+        {
+          'name': 'Taco de chorizo',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
         },
-        'menu': [
-          {
-            'name': 'Enchiladas',
-            'price': 7,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Guacamole',
-            'price': 15,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Tamales',
-            'price': 10,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          }
-        ]
+        {
+          'name': 'Tacos al Pastor',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Elote',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    },
+    {
+      'name': 'Como Agua para Chocolate',
+      'location': {
+        'coordinate x': 90,
+        'coordinate y': 3
       },
-      {
-        'name': 'Chili´s',
-        'location': {
-          'coordinate x': 90,
-          'coordinate y': 3
+      'menu': [
+        {
+          'name': 'Enchiladas',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
         },
-        'menu': [
-          {
-            'name': 'Chiles Rellenos',
-            'price': 7,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Sopa de Tortilla',
-            'price': 15,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Quesadillas',
-            'price': 10,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          }
-        ]
-      }
-    ]
-  },
-  'PERÚ': {
-    'restaurants': [
-      {
-        'name': 'Perú Gourmet',
-        'location': {
-          'coordinate x': 90,
-          'coordinate y': 3
+        {
+          'name': 'Guacamole',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
         },
-        'menu': [
-          {
-            'name': 'Ceviche',
-            'price': 7,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Anticuchos',
-            'price': 15,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Aji de Gallina',
-            'price': 10,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          }
-        ]
+        {
+          'name': 'Tamales',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    },
+    {
+      'name': 'Chili´s',
+      'location': {
+        'coordinate x': 90,
+        'coordinate y': 3
       },
-      {
-        'name': 'El Rinconcito Arequipeño',
-        'location': {
-          'coordinate x': 90,
-          'coordinate y': 3
+      'menu': [
+        {
+          'name': 'Chiles Rellenos',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
         },
-        'menu': [
-          {
-            'name': 'Rocoto relleno',
-            'price': 7,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Adobo',
-            'price': 15,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Cauche de queso',
-            'price': 10,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          }
-        ]
+        {
+          'name': 'Sopa de Tortilla',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Quesadillas',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    }
+  ],
+  'PERÚ': [
+    {
+      'name': 'Perú Gourmet',
+      'location': {
+        'coordinate x': 90,
+        'coordinate y': 3
       },
-      {
-        'name': 'Restaurant el Sillar',
-        'location': {
-          'coordinate x': 90,
-          'coordinate y': 3
+      'menu': [
+        {
+          'name': 'Ceviche',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
         },
-        'menu': [
-          {
-            'name': 'Escribano',
-            'price': 7,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Soltero de Queso',
-            'price': 15,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Chicharrón de Chancho',
-            'price': 10,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          }
-        ]
+        {
+          'name': 'Anticuchos',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Aji de Gallina',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    },
+    {
+      'name': 'El Rinconcito Arequipeño',
+      'location': {
+        'coordinate x': 90,
+        'coordinate y': 3
       },
-      {
-        'name': 'Rinconcito Punto y Sabor Arequipa',
-        'location': {
-          'coordinate x': 90,
-          'coordinate y': 3
+      'menu': [
+        {
+          'name': 'Rocoto relleno',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
         },
-        'menu': [
-          {
-            'name': 'Caldo Blanco',
-            'price': 7,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Ocopa Arequipeña',
-            'price': 15,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Ají de Calabaza',
-            'price': 10,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          }
-        ]
+        {
+          'name': 'Adobo',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Cauche de queso',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    },
+    {
+      'name': 'Restaurant el Sillar',
+      'location': {
+        'coordinate x': 90,
+        'coordinate y': 3
       },
-      {
-        'name': 'La Muralla',
-        'location': {
-          'coordinate x': 90,
-          'coordinate y': 3
+      'menu': [
+        {
+          'name': 'Escribano',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
         },
-        'menu': [
-          {
-            'name': 'Parihuela',
-            'price': 7,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Lomo Saltado',
-            'price': 15,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Aji de Gallina',
-            'price': 10,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          }
-        ]
+        {
+          'name': 'Soltero de Queso',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Chicharrón de Chancho',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    },
+    {
+      'name': 'Rinconcito Punto y Sabor Arequipa',
+      'location': {
+        'coordinate x': 90,
+        'coordinate y': 3
       },
-      {
-        'name': 'Rinconcito Punto y Sabor Arequipa',
-        'location': {
-          'coordinate x': 90,
-          'coordinate y': 3
+      'menu': [
+        {
+          'name': 'Caldo Blanco',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
         },
-        'menu': [
-          {
-            'name': 'Caldo Blanco',
-            'price': 7,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Ocopa Arequipeña',
-            'price': 15,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Ají de Calabaza',
-            'price': 10,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          }
-        ]
-      }
-    ]
-  },
-  'VENEZUELA': {
-    'restaurants': [
-      {
-        'name': 'Arepa Cafe',
-        'location': {
-          'coordinate x': 90,
-          'coordinate y': 3
+        {
+          'name': 'Ocopa Arequipeña',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
         },
-        'menu': [
-          {
-            'name': 'Arepa',
-            'price': 7,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Hallacas',
-            'price': 15,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Tizana',
-            'price': 10,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          }
-        ]
+        {
+          'name': 'Ají de Calabaza',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    },
+    {
+      'name': 'La Muralla',
+      'location': {
+        'coordinate x': 90,
+        'coordinate y': 3
       },
-      {
-        'name': 'El Budare Resto Bar',
-        'location': {
-          'coordinate x': 90,
-          'coordinate y': 3
+      'menu': [
+        {
+          'name': 'Parihuela',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
         },
-        'menu': [
-          {
-            'name': 'Arepas',
-            'price': 7,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'El Pabellón Criollo',
-            'price': 15,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          },
-          {
-            'name': 'Hallacas',
-            'price': 10,
-            'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-          }
-        ]
-      }
-    ]
-  }
+        {
+          'name': 'Lomo Saltado',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Aji de Gallina',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    },
+    {
+      'name': 'Rinconcito Punto y Sabor Arequipa',
+      'location': {
+        'coordinate x': 90,
+        'coordinate y': 3
+      },
+      'menu': [
+        {
+          'name': 'Caldo Blanco',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Ocopa Arequipeña',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Ají de Calabaza',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    }
+  ],
+  'VENEZUELA': [
+    {
+      'name': 'Arepa Cafe',
+      'location': {
+        'coordinate x': 90,
+        'coordinate y': 3
+      },
+      'menu': [
+        {
+          'name': 'Arepa',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Hallacas',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Tizana',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    },
+    {
+      'name': 'El Budare Resto Bar',
+      'location': {
+        'coordinate x': 90,
+        'coordinate y': 3
+      },
+      'menu': [
+        {
+          'name': 'Arepas',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'El Pabellón Criollo',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Hallacas',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    }
+  ]
 };
