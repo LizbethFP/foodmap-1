a1232f1c0ad23d1e9ed31f5c1a3df2e1c7385f78
Cintia Tahirih <tahysluana@gmail.com>
2017-12-25 17:23:21 -0500

    Añade cordenadas de ubicacion a la data

--- js/data.js
+++ js/data.js
@@ -2,10 +2,11 @@ var data = {
   'MEXICO': [
     {
       'name': 'Taqueria No Te Rajes',
-      'location': {
-        'coordinate x': 90,
-        'coordinate y': 3
+      'location': { 
+        'coordinate x': -12.125587,
+        'coordinate y': -77.021341
       },
+      'direction': 'Av. Alfredo Benavides 1083, Miraflores'
       'menu': [
         {
           'name': 'Taco de chorizo',
@@ -27,9 +28,10 @@ var data = {
     {
       'name': 'Como Agua para Chocolate',
       'location': {
-        'coordinate x': 90,
-        'coordinate y': 3
+        'coordinate x': -12.096833,
+        'coordinate y': -77.035490
       },
+      'direction': 'Pancho Fierro 108, San Isidro',
       'menu': [
         {
           'name': 'Enchiladas',
@@ -51,9 +53,60 @@ var data = {
     {
       'name': 'Chili´s',
       'location': {
-        'coordinate x': 90,
-        'coordinate y': 3
+        'coordinate x': -12.132703,
+        'coordinate y': -77.031631
       },
+      'direction': 'Larcomar, Malecón de la Reserva 610, Miraflores',
+      'menu': [
+        {
+          'name': 'Chiles Rellenos',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Sopa de Tortilla',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Quesadillas',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    }
+    {
+      'name': 'Chili´s',
+      'location': {
+        'coordinate x': -12.118573,
+        'coordinate y': -77.032809
+      },
+      'direction': 'Av. José Pardo 293, Miraflores',
+      'menu': [
+        {
+          'name': 'Chiles Rellenos',
+          'price': 7,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Sopa de Tortilla',
+          'price': 15,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        },
+        {
+          'name': 'Quesadillas',
+          'price': 10,
+          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+        }
+      ]
+    }
+    {
+      'name': 'Chili´s',
+      'location': {
+        'coordinate x': -12.112300,
+        'coordinate y': -77.011541
+      },
+      'direction': 'Av Tomás Marsano 961, Surquillo',
       'menu': [
         {
           'name': 'Chiles Rellenos',
@@ -77,9 +130,10 @@ var data = {
     {
       'name': 'Perú Gourmet',
       'location': {
-        'coordinate x': 90,
-        'coordinate y': 3
+        'coordinate x': -12.045560,
+        'coordinate y': -77.030645
       },
+      'direction': 'Pasaje Santa Rosa 165, Cercado de Lima',
       'menu': [
         {
           'name': 'Ceviche',
@@ -101,9 +155,10 @@ var data = {
     {
       'name': 'El Rinconcito Arequipeño',
       'location': {
-        'coordinate x': 90,
-        'coordinate y': 3
+        'coordinate x': -12.083779,
+        'coordinate y': -77.031331
       },
+      'direction': 'José Bernardo Alcedo 514, Lince';
       'menu': [
         {
           'name': 'Rocoto relleno',
@@ -125,9 +180,10 @@ var data = {
     {
       'name': 'Restaurant el Sillar',
       'location': {
-        'coordinate x': 90,
-        'coordinate y': 3
+        'coordinate x': -12.117631,
+        'coordinate y': -77.030284
       },
+      'direction': 'Calle Atahualpa 229, Miraflores',
       'menu': [
         {
           'name': 'Escribano',
@@ -149,9 +205,10 @@ var data = {
     {
       'name': 'Rinconcito Punto y Sabor Arequipa',
       'location': {
-        'coordinate x': 90,
-        'coordinate y': 3
+        'coordinate x': -12.084607,
+        'coordinate y': -77.030101
       },
+      'direction': 'Av. Francisco Lazo 2055, Lince',
       'menu': [
         {
           'name': 'Caldo Blanco',
@@ -173,9 +230,10 @@ var data = {
     {
       'name': 'La Muralla',
       'location': {
-        'coordinate x': 90,
-        'coordinate y': 3
+        'coordinate x': -12.052238,
+        'coordinate y': -77.034036
       },
+      'direction': 'Av. Nicolás de Piérola 1018, Cercado de Lima',
       'menu': [
         {
           'name': 'Parihuela',
@@ -194,38 +252,15 @@ var data = {
         }
       ]
     },
-    {
-      'name': 'Rinconcito Punto y Sabor Arequipa',
-      'location': {
-        'coordinate x': 90,
-        'coordinate y': 3
-      },
-      'menu': [
-        {
-          'name': 'Caldo Blanco',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Ocopa Arequipeña',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Ají de Calabaza',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
-    }
   ],
   'VENEZUELA': [
     {
       'name': 'Arepa Cafe',
       'location': {
-        'coordinate x': 90,
-        'coordinate y': 3
+        'coordinate x': -12.145215,
+        'coordinate y': -77.021681
       },
+      'direction': 'Av Almirante Miguel Grau N° 624, Barranco',
       'menu': [
         {
           'name': 'Arepa',
@@ -247,9 +282,10 @@ var data = {
     {
       'name': 'El Budare Resto Bar',
       'location': {
-        'coordinate x': 90,
-        'coordinate y': 3
+        'coordinate x': -12.145559,
+        'coordinate y': -77.018984
       },
+      'direction': 'Manuel Segura 123, Barranco',
       'menu': [
         {
           'name': 'Arepas',
