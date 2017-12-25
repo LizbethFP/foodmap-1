5763cae1dfba14d09a8c498eaebe496ef9e9d098
Cintia Tahirih <tahysluana@gmail.com>
2017-12-25 18:47:49 -0500

    Modifica Data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-
+// $(.title-splash)
--- js/data.js
+++ js/data.js
@@ -1,308 +1,433 @@
-var data = {
-  'MEXICO': [
-    {
-      'name': 'Taqueria No Te Rajes',
-      'location': { 
-        'coordinate x': -12.125587,
-        'coordinate y': -77.021341
-      },
-      'direction': 'Av. Alfredo Benavides 1083, Miraflores'
-      'menu': [
-        {
-          'name': 'Taco de chorizo',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Tacos al Pastor',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Elote',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
+var data = [
+  {
+    'name': 'Restaurante El Cardenal',
+    'location': {
+      'coordinate x': 19.347198,
+      'coordinate y': -99.188840
     },
-    {
-      'name': 'Como Agua para Chocolate',
-      'location': {
-        'coordinate x': -12.096833,
-        'coordinate y': -77.035490
+    'country': 'Mexico',
+    'direction': 'Av. de la Paz # 32 | Col. San Ángel, Ciudad de México',
+    'menu': [
+      {
+        'name': 'Taco de chorizo',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
       },
-      'direction': 'Pancho Fierro 108, San Isidro',
-      'menu': [
-        {
-          'name': 'Enchiladas',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Guacamole',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Tamales',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
+      {
+        'name': 'Tacos al Pastor',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Elote',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'Trattoria della Casa Nuova',
+    'type food': 'Italiana',
+    'location': {
+      'coordinate x': 19.346844,
+      'coordinate y': -99.188971
     },
-    {
-      'name': 'Chili´s',
-      'location': {
-        'coordinate x': -12.132703,
-        'coordinate y': -77.031631
+    'country': 'Mexico',
+    'direction': 'Avenida de la Paz 40, San Ángel, Ciudad de México',
+    'menu': [
+      {
+        'name': 'Insalata Di Gambas',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
       },
-      'direction': 'Larcomar, Malecón de la Reserva 610, Miraflores',
-      'menu': [
-        {
-          'name': 'Chiles Rellenos',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Sopa de Tortilla',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Quesadillas',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
-    }
-    {
-      'name': 'Chili´s',
-      'location': {
-        'coordinate x': -12.118573,
-        'coordinate y': -77.032809
+      {
+        'name': 'Mac & Cheese con Langostinos',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
       },
-      'direction': 'Av. José Pardo 293, Miraflores',
-      'menu': [
-        {
-          'name': 'Chiles Rellenos',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Sopa de Tortilla',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Quesadillas',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
-    }
-    {
-      'name': 'Chili´s',
-      'location': {
-        'coordinate x': -12.112300,
-        'coordinate y': -77.011541
-      },
-      'direction': 'Av Tomás Marsano 961, Surquillo',
-      'menu': [
-        {
-          'name': 'Chiles Rellenos',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Sopa de Tortilla',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Quesadillas',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
-    }
-  ],
-  'PERÚ': [
-    {
-      'name': 'Perú Gourmet',
-      'location': {
-        'coordinate x': -12.045560,
-        'coordinate y': -77.030645
-      },
-      'direction': 'Pasaje Santa Rosa 165, Cercado de Lima',
-      'menu': [
-        {
-          'name': 'Ceviche',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Anticuchos',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Aji de Gallina',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
+      {
+        'name': 'Spaghetti al pomodore e oro',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'Perú Gourmet',
+    'type food': 'Peruana',
+    'location': {
+      'coordinate x': -12.045560,
+      'coordinate y': -77.030645
     },
-    {
-      'name': 'El Rinconcito Arequipeño',
-      'location': {
-        'coordinate x': -12.083779,
-        'coordinate y': -77.031331
+    'country': 'Perú',
+    'direction': 'Pasaje Santa Rosa 165, Cercado de Lima',
+    'menu': [
+      {
+        'name': 'Ceviche',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
       },
-      'direction': 'José Bernardo Alcedo 514, Lince';
-      'menu': [
-        {
-          'name': 'Rocoto relleno',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Adobo',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Cauche de queso',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
+      {
+        'name': 'Anticuchos',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Aji de Gallina',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'El Rinconcito Arequipeño',
+    'type food': 'Peruana',
+    'location': {
+      'coordinate x': -12.083779,
+      'coordinate y': -77.031331
     },
-    {
-      'name': 'Restaurant el Sillar',
-      'location': {
-        'coordinate x': -12.117631,
-        'coordinate y': -77.030284
+    'country': 'Perú',
+    'direction': 'José Bernardo Alcedo 514, Lince',
+    'menu': [
+      {
+        'name': 'Rocoto relleno',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
       },
-      'direction': 'Calle Atahualpa 229, Miraflores',
-      'menu': [
-        {
-          'name': 'Escribano',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Soltero de Queso',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Chicharrón de Chancho',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
+      {
+        'name': 'Adobo',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Cauche de queso',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'Restaurant el Sillar',
+    'type food': 'Peruana',
+    'location': {
+      'coordinate x': -12.117631,
+      'coordinate y': -77.030284
     },
-    {
-      'name': 'Rinconcito Punto y Sabor Arequipa',
-      'location': {
-        'coordinate x': -12.084607,
-        'coordinate y': -77.030101
+    'country': 'Perú',
+    'direction': 'Calle Atahualpa 229, Miraflores',
+    'menu': [
+      {
+        'name': 'Escribano',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
       },
-      'direction': 'Av. Francisco Lazo 2055, Lince',
-      'menu': [
-        {
-          'name': 'Caldo Blanco',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Ocopa Arequipeña',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Ají de Calabaza',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
+      {
+        'name': 'Soltero de Queso',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Chicharrón de Chancho',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'Rinconcito Punto y Sabor Arequipa',
+    'type food': 'Peruana',
+    'location': {
+      'coordinate x': -12.084607,
+      'coordinate y': -77.030101
     },
-    {
-      'name': 'La Muralla',
-      'location': {
-        'coordinate x': -12.052238,
-        'coordinate y': -77.034036
+    'country': 'Perú',
+    'direction': 'Av. Francisco Lazo 2055, Lince',
+    'menu': [
+      {
+        'name': 'Caldo Blanco',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
       },
-      'direction': 'Av. Nicolás de Piérola 1018, Cercado de Lima',
-      'menu': [
-        {
-          'name': 'Parihuela',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Lomo Saltado',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Aji de Gallina',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
+      {
+        'name': 'Ocopa Arequipeña',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Ají de Calabaza',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'La Muralla',
+    'type food': 'Peruana',
+    'location': {
+      'coordinate x': -12.052238,
+      'coordinate y': -77.034036
     },
-  ],
-  'VENEZUELA': [
-    {
-      'name': 'Arepa Cafe',
-      'location': {
-        'coordinate x': -12.145215,
-        'coordinate y': -77.021681
+    'country': 'Perú',
+    'direction': 'Av. Nicolás de Piérola 1018, Cercado de Lima',
+    'menu': [
+      {
+        'name': 'Parihuela',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
       },
-      'direction': 'Av Almirante Miguel Grau N° 624, Barranco',
-      'menu': [
-        {
-          'name': 'Arepa',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Hallacas',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Tizana',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
+      {
+        'name': 'Lomo Saltado',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Aji de Gallina',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'Taqueria No Te Rajes',
+    'type food': 'Mexicana',
+    'location': {
+      'coordinate x': -12.125587,
+      'coordinate y': -77.021341
     },
-    {
-      'name': 'El Budare Resto Bar',
-      'location': {
-        'coordinate x': -12.145559,
-        'coordinate y': -77.018984
+    'country': 'Perú',
+    'direction': 'Av. Alfredo Benavides 1083, Miraflores',
+    'menu': [
+      {
+        'name': 'Taco de chorizo',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
       },
-      'direction': 'Manuel Segura 123, Barranco',
-      'menu': [
-        {
-          'name': 'Arepas',
-          'price': 7,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'El Pabellón Criollo',
-          'price': 15,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        },
-        {
-          'name': 'Hallacas',
-          'price': 10,
-          'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
-        }
-      ]
-    }
-  ]
-};
+      {
+        'name': 'Tacos al Pastor',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Elote',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'Como Agua para Chocolate',
+    'type food': 'Mexicana',
+    'location': {
+      'coordinate x': -12.096833,
+      'coordinate y': -77.035490
+    },
+    'country': 'Perú',
+    'direction': 'Pancho Fierro 108, San Isidro',
+    'menu': [
+      {
+        'name': 'Enchiladas',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Guacamole',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Tamales',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'Chili´s',
+    'type food': 'Mexicana',
+    'location': {
+      'coordinate x': -12.132703,
+      'coordinate y': -77.031631
+    },
+    'country': 'Perú',
+    'direction': 'Larcomar, Malecón de la Reserva 610, Miraflores',
+    'menu': [
+      {
+        'name': 'Chiles Rellenos',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Sopa de Tortilla',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Quesadillas',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'Chili´s',
+    'type food': 'Mexicana',
+    'location': {
+      'coordinate x': -12.118573,
+      'coordinate y': -77.032809
+    },
+    'country': 'Perú',
+    'direction': 'Av. José Pardo 293, Miraflores',
+    'menu': [
+      {
+        'name': 'Chiles Rellenos',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Sopa de Tortilla',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Quesadillas',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'Chili´s',
+    'type food': 'Mexicana',
+    'location': {
+      'coordinate x': -12.112300,
+      'coordinate y': -77.011541
+    },
+    'country': 'Perú',
+    'direction': 'Av Tomás Marsano 961, Surquillo',
+    'menu': [
+      {
+        'name': 'Chiles Rellenos',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Sopa de Tortilla',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Quesadillas',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'Arepa Cafe',
+    'type food': 'Venezolana',
+    'location': {
+      'coordinate x': -12.145215,
+      'coordinate y': -77.021681
+    },
+    'country': 'Perú',
+    'direction': 'Av Almirante Miguel Grau N° 624, Barranco',
+    'menu': [
+      {
+        'name': 'Arepa',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Hallacas',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Tizana',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'El Budare Resto Bar',
+    'type food': 'Venezolana',
+    'location': {
+      'coordinate x': -12.145559,
+      'coordinate y': -77.018984
+    },
+    'country': 'Perú',
+    'direction': 'Manuel Segura 123, Barranco',
+    'menu': [
+      {
+        'name': 'Arepas',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'El Pabellón Criollo',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Hallacas',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'La Bodega de la Trattoria',
+    'type food': 'Italiana',
+    'location': {
+      'coordinate x': -12.1119517,
+      'coordinate y': -77.033144
+    },
+    'country': 'Perú',
+    'direction': 'Calle General Borgoño 748, Miraflores',
+    'menu': [
+      {
+        'name': 'Insalata Di Gambas',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Mac & Cheese con Langostinos',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Spaghetti al pomodore e oro',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  },
+  {
+    'name': 'La Bodega de la Trattoria',
+    'type food': 'Italiana',
+    'location': {
+      'coordinate x': -12.076449,
+      'coordinate y': -77.082461
+    },
+    'country': 'Perú',
+    'direction': 'Plaza San Miguel, Av. La Marina 2000, San Miguel',
+    'menu': [
+      {
+        'name': 'Insalata Di Gambas',
+        'price': 7,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Mac & Cheese con Langostinos',
+        'price': 15,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      },
+      {
+        'name': 'Spaghetti al pomodore e oro',
+        'price': 10,
+        'photo': 'http://dummyimage.com/133x195.png/cc0000/ffffff'
+      }
+    ]
+  }
+];
