9bdf554ac8702410b93e09f84328878e1275dc18
Cintia Tahirih <tahysluana@gmail.com>
2017-12-25 21:39:38 -0500

    modifica splash screen

--- js/app.js
+++ js/app.js
@@ -1,47 +1,44 @@
-$(document).ready(function () {
-	// splash screen
-+function () {
-  var intervals = []
+$(document).ready(function() {
+  //  splash screen
+  function splash() {
+    var intervals = [];
 
-  intervals.push(setInterval(function () {
-    $('.umbel-logo .animated').each(function () {
-      this.classList.remove('animated')
-      this.offsetHeight
-      this.classList.add('animated')
-    })
-  }, 2000))
+    intervals.push(setInterval(function() {
+      $('.foodmap-logo .animated').each(function() {
+        this.classList.remove('animated');
+        this.offsetHeight;
+        this.classList.add('animated');
+      });
+    }, 2000));
 
+    var appearPhase = true;
+    var $text = $('.loading-text');
 
-  var appearPhase = true;
-  var $text = $('.loading-text')
+    var togglePhase = function() {
+      appearPhase = !appearPhase;
+    };
 
-  var togglePhase = function () {
-    appearPhase = !appearPhase
+    intervals.push(setInterval(function() {
+      var selector = appearPhase ? '.letter:not(.visible)' : '.letter.visible';
+      var $letters = $text.find(selector);
+
+      $letters.eq(~~(Math.random() * $letters.length)).toggleClass('visible', appearPhase);
+      $letters.length - 1 || setTimeout(togglePhase, 2000);
+    }, 300));
+
+    function destroy() {
+      $('.loading-splash').remove();
+      intervals.forEach(function(interval) {
+        clearInterval(interval);
+      });
+    }
+
+    $(window).on('loadComplete.um', destroy);
   }
 
-  intervals.push(setInterval(function () {
-    var selector = appearPhase ? '.letter:not(.visible)' : '.letter.visible'
-    var $letters = $text.find(selector)
+  splash();
 
-    $letters
-      .eq(~~(Math.random() * $letters.length))
-      .toggleClass('visible', appearPhase)
-
-    $letters.length - 1 || setTimeout(togglePhase, 2000)
-  }, 300))
-  
-  function destroy() {
-    $('.loading-splash').remove()
-    intervals.forEach(function(interval) {
-      clearInterval(interval)
-    })
-  }
-  
-  $(window).on('loadComplete.um', destroy)
-}()
-
-setTimeout(function () {
-	$(".splash-screen").fadeOut(1500);
-}, 3000);
-
+  setTimeout(function() {
+    $('#splash-screen').fadeOut(1500);
+  }, 3000);
 });
