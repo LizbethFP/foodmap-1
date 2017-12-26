e3ea5579bb68ad12578c9ba9c76b89bc2b9ed4bb
Cintia Tahirih <tahysluana@gmail.com>
2017-12-26 17:45:05 -0500

    Funciones para obtener ubicación de los restaurants

--- js/app.js
+++ js/app.js
@@ -31,41 +31,62 @@ $(document).ready(function() {
 
   setTimeout(function() {
     $('#splash-screen').fadeOut(1500);
-  }, 3000);  
+  }, 3000);
 });
 
 // geolocalización
-  function initMap() {
-    var map = new google.maps.Map($('#map')[0], {
-      center: {	lat: -34.397,
+function initMap() {
+  var map = new google.maps.Map($('#map')[0], {
+    center: {	lat: -34.397,
         		lng: 150.644},
-      			zoom: 6
+      			zoom: 10
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
     });
-    var infoWindow = new google.maps.InfoWindow({map: map});
-
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
   }
+}
 
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
-      'Error: Your browser doesn\'t support geolocation.');
-  }  
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+}
+
+var coordinates = [];
+var labels = [];
+
+for (var i in data) {
+  coordinates.push(data[i].location);
+  labels.push(data[i].name);
+}
+
+var markers = locations.map(function(location, i) {
+  return new google.maps.Marker({
+    position: location,
+    label: labels[i]
+  });
+});
+
+// Add a marker clusterer to manage the markers.
+var markerCluster = new MarkerClusterer(map, markers,
+  {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+var locations = coordinates;
